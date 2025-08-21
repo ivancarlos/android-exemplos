8189f3957037dee2b60ee32a808ff9951517de42
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-08-21 14:16:19 -0300

    :style: Atualiza o estilo do arquivo andengine/local.sh

--- andengine/local.sh
+++ andengine/local.sh
@@ -4,7 +4,7 @@ for d in AndEngine*/ examples/*/; do
     if [[ -d "$d" ]]; then
         rel="../local.properties"
         [[ "$d" == examples/*/ ]] && rel="../../local.properties"
-        echo ln -s "$rel" "${d}local.properties"
+        ln -s "$rel" "${d}local.properties"
     fi
 done
 
