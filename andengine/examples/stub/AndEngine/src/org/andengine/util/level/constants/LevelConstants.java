0a979897d845d5f176bf3410af54365d37dc0ac3
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-11-10 08:21:50 -0300

    :add: Adiciona o arquivo andengine/examples/stub/AndEngine/src/org/andengine/util/level/constants/LevelConstants.java

---
+++ andengine/examples/stub/AndEngine/src/org/andengine/util/level/constants/LevelConstants.java
@@ -0,0 +1,41 @@
+package org.andengine.util.level.constants;
+
+
+/**
+ * (c) 2010 Nicolas Gramlich
+ * (c) 2011 Zynga Inc.
+ *
+ * @author Nicolas Gramlich
+ * @since 14:23:27 - 11.10.2010
+ */
+public interface LevelConstants {
+	// ===========================================================
+	// Constants
+	// ===========================================================
+
+	public static final String TAG_LEVEL = "level";
+	public static final String TAG_LEVEL_ATTRIBUTE_NAME = "name";
+	public static final String TAG_LEVEL_ATTRIBUTE_UID = "uid";
+	public static final String TAG_LEVEL_ATTRIBUTE_WIDTH = "width";
+	public static final String TAG_LEVEL_ATTRIBUTE_HEIGHT = "height";
+
+	public static final String TAG_LEVEL_ENTITY_ATTRIBUTE_POSITION_X = "x";
+	public static final String TAG_LEVEL_ENTITY_ATTRIBUTE_POSITION_Y = "y";
+	public static final String TAG_LEVEL_ENTITY_ATTRIBUTE_WIDTH = "width";
+	public static final String TAG_LEVEL_ENTITY_ATTRIBUTE_HEIGHT = "height";
+	public static final String TAG_LEVEL_ENTITY_ATTRIBUTE_SCALE_X = "scale_x";
+	public static final String TAG_LEVEL_ENTITY_ATTRIBUTE_SCALE_Y = "scale_y";
+	public static final String TAG_LEVEL_ENTITY_ATTRIBUTE_ROTATION = "rotation";
+	public static final String TAG_LEVEL_ENTITY_ATTRIBUTE_ZINDEX = "zindex";
+	public static final String TAG_LEVEL_ENTITY_ATTRIBUTE_COLOR_R = "color_r";
+	public static final String TAG_LEVEL_ENTITY_ATTRIBUTE_COLOR_G = "color_g";
+	public static final String TAG_LEVEL_ENTITY_ATTRIBUTE_COLOR_B = "color_b";
+	public static final String TAG_LEVEL_ENTITY_ATTRIBUTE_FLIP_HOR = "flip_hor";
+	public static final String TAG_LEVEL_ENTITY_ATTRIBUTE_FLIP_VER= "flip_ver";
+	public static final String TAG_LEVEL_ENTITY_ATTRIBUTE_BLEND_SOURCE = "blend_src";
+	public static final String TAG_LEVEL_ENTITY_ATTRIBUTE_BLEND_DESTINATION = "blend_dst";
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+}
